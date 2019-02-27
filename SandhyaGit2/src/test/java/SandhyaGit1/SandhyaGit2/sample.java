9961dd2ebca3392ac514d3afba3c7c62a50604b9
sandhyakishore <sandysiriginedi@gmail.com>
2019-02-27 16:50:02 +0530

    myBranch 2

--- SandhyaGit2/src/test/java/SandhyaGit1/SandhyaGit2/sample.java
+++ SandhyaGit2/src/test/java/SandhyaGit1/SandhyaGit2/sample.java
@@ -5,7 +5,8 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("THis is my first commit");
-
+		System.out.println("THis is my second commit");
+		System.out.println("THis is my third commit");
 	}
 
 }
